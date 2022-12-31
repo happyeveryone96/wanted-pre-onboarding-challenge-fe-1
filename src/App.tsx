a52f7f47a0b762dd45a6455d1323284d0ef5682d
happyeveryone96 <happyeveryone@naver.com>
2022-12-31 23:39:48 +0900

    feat: 로그인 및 회원가입 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
